54ba1cbc04a8af8de6e82b847a1ebe3b04130687
gabrielekvietinskaite <gabriele.kvietinskaite@gmail.com>
2021-01-08 20:54:54 +0200

    Add Iban controller

---
+++ IBAN-Verificator/IBAN-Verificator/Controllers/IbanController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IBAN_Verificator.Controllers
+{
+    [ApiController]
+    [Route("api/iban")]
+    public class IbanController : ControllerBase
+    {
+        public IbanController()
+        {
+
+        }
+    }
+}
